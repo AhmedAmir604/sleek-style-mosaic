5156ffd2bc5c006f1c717ddf5bdfc9931ab3e695
Ahmed <ahmedanari2001@gmail.com>
2025-01-12 03:37:32 +0500

    almost done

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -5,6 +5,8 @@ export const ProductImage = () => {
     <div className="relative">
       <div className="absolute -top-20 -left-20 w-40 h-40 bg-nike-gray/20 rounded-full blur-xl " />
       <div className="absolute bottom-0 right-0 w-32 h-32 bg-nike-gray/20 rounded-full blur-lg " />
+      <div className="absolute top-40 left-20 w-40 h-40 bg-nike-gray/20 rounded-full blur-lg " />
+      <div className="absolute right-0 w-32 h-32 bg-nike-gray/20 rounded-full blur-xl" />
       <img
         src="/lovable-uploads/NikeShoe.png"
         alt="Nike Air Max 90"
